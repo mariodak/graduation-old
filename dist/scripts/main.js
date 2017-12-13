b4e116ca443711944113fa6996f5ee21902c5f9a
mariodak <e.mariodak@gmail.com>
2017-12-13 19:02:31 +0100

    update-shit

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Active menu button
 $('#button').click(function () {
   return $('.mobile-nav-show').toggleClass('active-button');
 });
+//$('#button').click(() => $('.mobile-nav-show').slideToggle())
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,3 @@
+// Active menu button
 $('#button').click(() => $('.mobile-nav-show').toggleClass('active-button'))
+//$('#button').click(() => $('.mobile-nav-show').slideToggle())
